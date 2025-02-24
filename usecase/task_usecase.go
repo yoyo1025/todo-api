aa950c1f7fc4074342bb4c44437e23a47d8facfc
yoyo1025 <hsjejxys@gmail.com>
2025-02-24 14:51:10 +0900

    ユースケース層のエラーを変更

--- usecase/task_usecase.go
+++ usecase/task_usecase.go
@@ -28,7 +28,7 @@ func NewTaskUsecase(taskRepository repository.ITaskRepository) ITaskUsecase{
 func (tu *TaskUsecase) FindAllTask(c echo.Context, userId int64) ([]*model.Task, error) {
 	tasks, err := tu.taskRepository.FindAll(userId)
 	if err != nil {
-		return nil, c.JSON(http.StatusInternalServerError, "タスクの取得に失敗")
+		return nil, echo.NewHTTPError(http.StatusInternalServerError, "タスクの取得に失敗")
 	}
 	return tasks, nil
 }
@@ -37,7 +37,7 @@ func (tu *TaskUsecase) FindAllTask(c echo.Context, userId int64) ([]*model.Task,
 func (tu *TaskUsecase) CreateTask(c echo.Context, userId int64, title, detail string, status int64) error {
 	task := model.NewTask(userId, title, detail, status)
 	if err := tu.taskRepository.Create(task); err != nil {
-		return c.JSON(http.StatusInternalServerError, "タスクの作成に失敗")
+		return echo.NewHTTPError(http.StatusInternalServerError, "タスクの作成に失敗")
 	}
 	return nil
 }
@@ -46,7 +46,7 @@ func (tu *TaskUsecase) CreateTask(c echo.Context, userId int64, title, detail st
 func (tu *TaskUsecase) UpdateTask(c echo.Context, userId int64, title, detail string, status int64) error {
 	updatedTask := model.NewTask(userId, title, detail, status)
 	if err := tu.taskRepository.Update(updatedTask); err != nil {
-		return c.JSON(http.StatusInternalServerError, "タスクの更新に失敗")
+		return echo.NewHTTPError(http.StatusInternalServerError, "タスクの更新に失敗")
 	}
 	return nil
 }
